bd7efded782cdc738c0b47894e5cfed47e219620
M Miller <miller.2676@osu.edu>
2019-03-06 15:11:19 -0800

    begin to build out app; add movie component

--- src/App.js
+++ src/App.js
@@ -2,24 +2,32 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Movie from './Movie'
+
+const movies = [
+  {
+    id: 1,
+    title: '10 Things I Hate About You',
+  },
+  {
+    id: 2,
+    title: 'I, Tonya',
+  },
+  {
+    id: 3,
+    title: 'The Big Sick',
+  },
+];
+
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
+        {movies.map(movie => <Movie key={movie.id} movie={movie} />)}
       </div>
     );
   }
