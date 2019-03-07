9b6eac8c2c79bec1ab07aa3cf96906d43578aeaf
M Miller <miller.2676@osu.edu>
2019-03-06 16:00:30 -0800

    pull data from themoviedb api

--- src/App.js
+++ src/App.js
@@ -4,30 +4,33 @@ import './App.css';
 
 import Movie from './Movie'
 
-const movies = [
-  {
-    id: 1,
-    title: '10 Things I Hate About You',
-  },
-  {
-    id: 2,
-    title: 'I, Tonya',
-  },
-  {
-    id: 3,
-    title: 'The Big Sick',
-  },
-];
-
-
 class App extends Component {
+
+  // set default state
+  state = {
+    movies: []
+  }
+
+  // pull data from api
+  async componentDidMount() {
+    try {
+      const res = await fetch('https://api.themoviedb.org/3/discover/movie?api_key=bd6c30d59224151decd977c2b20d4b48&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1');
+      const movies = await res.json();
+      this.setState({
+        movies: movies.results
+      })
+    } catch(e) {
+      console.log(e);
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
         </header>
-        {movies.map(movie => <Movie key={movie.id} movie={movie} />)}
+        {this.state.movies.map(movie => <Movie key={movie.id} movie={movie} />)}
       </div>
     );
   }
--- src/Movie.js
+++ src/Movie.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export default class Movie extends Component {
+
+  static propTypes = {
+    movie: PropTypes.shape({
+      title: PropTypes.string.isRequired,
+    }),
+  }
+
   render() {
     return (
       <div>
-        {this.props.movie.title}
+        <h3>{this.props.movie.title}</h3>
       </div>
     )
   }
