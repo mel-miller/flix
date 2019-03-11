9b41f2f83197b7d0a3f55411489afd293b309b89
M Miller <miller.2676@osu.edu>
2019-03-11 13:19:04 -0700

    begin to set up reducers

--- src/App.js
+++ src/App.js
@@ -2,26 +2,35 @@ import React from 'react';
 import {
   BrowserRouter as Router, Route, Switch, Link,
 } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
 import logo from './logo.svg';
 import './App.css';
+
+import rootReducer from './rootReducer';
 
 import MoviesList from './MoviesList';
 import MovieDetail from './MovieDetail';
 
+const store = createStore(rootReducer);
+
 const App = () => (
-  <Router>
-    <div className="App">
-      <header className="App-header">
-        <Link to="/">
-          <img src={logo} className="App-logo" alt="logo" />
-        </Link>
-      </header>
-      <Switch>
-        <Route exact path="/" component={MoviesList} />
-        <Route path="/:id" component={MovieDetail} />
-      </Switch>
-    </div>
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <Link to="/">
+            <img src={logo} className="App-logo" alt="logo" />
+          </Link>
+        </header>
+        <Switch>
+          <Route exact path="/" component={MoviesList} />
+          <Route path="/:id" component={MovieDetail} />
+        </Switch>
+      </div>
+    </Router>
+  </Provider>
 );
 
 export default App;
