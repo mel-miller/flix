279c06d9ce28fb8710fe12aacc9833996d8d7cd6
M Miller <miller.2676@osu.edu>
2019-03-11 13:28:21 -0700

    install and configure redux-devtools-extension

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {
 } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 import logo from './logo.svg';
 import './App.css';
@@ -13,7 +14,11 @@ import rootReducer from './rootReducer';
 import MoviesList from './MoviesList';
 import MovieDetail from './MovieDetail';
 
-const store = createStore(rootReducer);
+const store = createStore(
+  rootReducer,
+  {},
+  composeWithDevTools(),
+);
 
 const App = () => (
   <Provider store={store}>
