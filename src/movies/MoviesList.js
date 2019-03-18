84da65453129a9b348e87374c4752616486e55b7
M Miller <miller.2676@osu.edu>
2019-03-18 16:58:50 -0700

    reconfigure how movies are loaded

--- src/movies/MoviesList.js
+++ src/movies/MoviesList.js
@@ -1,30 +1,20 @@
 import React, { PureComponent } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 import styled from 'styled-components';
 
 import Movie from './Movie';
+import { getMovies } from './actions';
 
 
 class MoviesList extends PureComponent {
-  // set default state
-  state = {
-    movies: [],
-  }
-
-  // pull data from api
-  async componentDidMount() {
-    try {
-      const res = await fetch('https://api.themoviedb.org/3/discover/movie?api_key=bd6c30d59224151decd977c2b20d4b48&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1');
-      const movies = await res.json();
-      this.setState({
-        movies: movies.results,
-      });
-    } catch (e) {
-      console.log(e); // eslint-disable-line
-    }
+  componentDidMount() {
+    const { getMovies } = this.props;
+    getMovies();
   }
 
   render() {
-    const { movies } = this.state;
+    const { movies } = this.props;
     return (
       <MovieGrid>
         {movies.map(movie => <Movie key={movie.id} movie={movie} />)}
@@ -33,7 +23,15 @@ class MoviesList extends PureComponent {
   }
 }
 
-export default MoviesList;
+const mapStateToProps = state => ({
+  movies: state.movies.movies,
+});
+
+const mapDispatchToProps = dispatch => bindActionCreators({
+  getMovies,
+}, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(MoviesList);
 
 const MovieGrid = styled.div`
   display: flex;
--- src/toggle/Toggle.js
+++ src/toggle/Toggle.js
@@ -3,18 +3,15 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
 import { toggleMessage } from './actions';
-import { getMovies } from '../movies/actions';
 
-const Toggle = ({ messageVisibility, toggleMessage, getMovies }) => (
+
+const Toggle = ({ messageVisibility, toggleMessage }) => (
   <div>
     {messageVisibility
       && <p>You will be seeing this if redux action is toggled</p>
     }
     <button type="button" onClick={toggleMessage}>
       Toggle Me
-    </button>
-    <button type="button" onClick={getMovies}>
-      Load Movies
     </button>
   </div>
 );
@@ -25,7 +22,6 @@ const mapStateToProps = state => ({
 
 const mapDispatchToProps = dispatch => bindActionCreators({
   toggleMessage,
-  getMovies,
 }, dispatch);
 
 export default connect(mapStateToProps, mapDispatchToProps)(Toggle);
