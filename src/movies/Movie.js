f53497dca8dbd21348e6661e3e79b93f8d9e02ca
M Miller <miller.2676@osu.edu>
2019-03-18 16:41:09 -0700

    clean up errors in movie.js

--- src/movies/Movie.js
+++ src/movies/Movie.js
@@ -8,8 +8,8 @@ const POSTER_PATH = 'http://image.tmdb.org/t/p/w154';
 
 const Movie = ({ movie }) => (
   <Link to={`/${movie.id}`}>
-    <Overdrive id={movie.id}>
-      <Poster src={`${POSTER_PATH}${movie.poster_path}`} alt="movie.title" />
+    <Overdrive id={`${movie.id}`}>
+      <Poster src={`${POSTER_PATH}${movie.poster_path}`} alt={movie.title} />
     </Overdrive>
   </Link>
 );
