d5c08f2b5178cec01811e75c868e35f157728c48
M Miller <miller.2676@osu.edu>
2019-03-18 18:18:06 -0700

    save movies list to local storage with 1 hour limit

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { applyMiddleware, createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
+import { save, load } from 'redux-localstorage-simple';
 
 import logo from './logo.svg';
 import './App.css';
@@ -21,8 +22,8 @@ const middleware = [logger, thunk];
 
 const store = createStore(
   rootReducer,
-  {},
-  composeWithDevTools(applyMiddleware(...middleware)),
+  load(),
+  composeWithDevTools(applyMiddleware(...middleware, save())),
 );
 
 
